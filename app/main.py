5165b55c1ee63204b707a8df667d55b7d8a7f28e
kboro <kkborowy@gmail.com>
2024-07-09 17:52:31 +0200

    include users_router

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
 from .routes.auth import router as auth_router
+from .routes.users import router as users_router
 
 app = FastAPI()
 
+app.include_router(users_router, prefix='/users')
 app.include_router(auth_router, prefix='/auth')
+
 
 @app.get('/')
 def read_root():
