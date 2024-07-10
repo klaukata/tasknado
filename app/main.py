21f659a5f230f409dec67bb1427d56d342ec03d3
kboro <kkborowy@gmail.com>
2024-07-10 12:42:50 +0200

    include task router

--- app/main.py
+++ app/main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 from .routes.auth import router as auth_router
 from .routes.users import router as users_router
+from .routes.tasks import router as task_router
 
 app = FastAPI()
 
 app.include_router(users_router, prefix='/users')
 app.include_router(auth_router, prefix='/auth')
+app.include_router(task_router, prefix='/task')
 
 
 @app.get('/')
