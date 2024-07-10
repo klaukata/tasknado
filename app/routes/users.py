05a829a3c107671d3d53b0caebd5be84c43441a3
kboro <kkborowy@gmail.com>
2024-07-10 12:40:43 +0200

    get req returns only users username and email

--- app/routes/users.py
+++ app/routes/users.py
@@ -1,8 +1,11 @@
 from fastapi import APIRouter, Depends
 from ..token import o2auth_scheme, decode_token
+from ..crud import get_user, display_user_details
+
 router = APIRouter()
 
 @router.get('/me')
 def read_logged_user(token: str = Depends(o2auth_scheme)):
-    decoded = decode_token(token)
-    return decoded
+    decoded_username = decode_token(token)
+    user_doc = get_user(decoded_username)
+    return display_user_details(user_doc)
